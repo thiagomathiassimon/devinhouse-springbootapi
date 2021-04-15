51688c8b9b4c10d21997023c5175cc5d7e624a01
Jefferson Henrique <jeffersonoh@gmail.com>
2021-04-14 21:23:18 -0300

    Aula 16/04 modulo 2 semana 6

--- src/main/java/br/com/devinhouse/springbootapi/service/ProcessoService.java
+++ src/main/java/br/com/devinhouse/springbootapi/service/ProcessoService.java
@@ -1,5 +1,6 @@
 package br.com.devinhouse.springbootapi.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,79 @@ public class ProcessoService {
 	@Autowired
 	private ProcessoRepository repository;
 
+	private List<ProcessoDTO> recuperarTodosProcesso() {
+		return repository.findAllProcessos();
+	}
+
 	public List<ProcessoDTO> recuperarProcessosMockados() {
 
 		// AQUI SERIA ONDE COLOCARIAMOS AS NOSSAS REGRAS DE NEGOCIO
 
-		return repository.findAllProcessos();
+		return recuperarTodosProcesso();
+	}
+
+	public List<ProcessoDTO> recuperarProcessosMockados(String ano) {
+
+		List<ProcessoDTO> todosProcessos = recuperarTodosProcesso();
+		List<ProcessoDTO> listProcessoFiltrados = new ArrayList<ProcessoDTO>();
+
+		for (ProcessoDTO processoDTO : todosProcessos) {
+
+			if (ano.equals(processoDTO.getNuAnoProcesso())) {
+				listProcessoFiltrados.add(processoDTO);
+			}
+
+		}
+
+		return listProcessoFiltrados;
+	}
+
+	public List<ProcessoDTO> cadastrarProcesso(ProcessoDTO processo) {
+
+		List<ProcessoDTO> todosProcessos = recuperarTodosProcesso();
+
+		todosProcessos.add(processo);
+
+		return todosProcessos;
+	}
+
+	public List<ProcessoDTO> atualizarProcesso(Integer nuProcesso, ProcessoDTO newProcesso) {
+
+		List<ProcessoDTO> todosProcessos = recuperarTodosProcesso();
+
+		for (ProcessoDTO processoDTO : todosProcessos) {
+
+			if (nuProcesso == processoDTO.getNuProcesso()) {
+				processoDTO.setDescricao(newProcesso.getDescricao());
+			}
+
+		}
+		return todosProcessos;
+	}
+
+	public List<ProcessoDTO> recuperarProcessosMockadosFiltrados(Integer inicio_id, Integer final_id) {
+
+		List<ProcessoDTO> todosProcessos = recuperarTodosProcesso();
+		List<ProcessoDTO> listProcessoFiltrados = new ArrayList<ProcessoDTO>();
+
+		for (ProcessoDTO processoDTO : todosProcessos) {
+
+			if (processoDTO.getNuProcesso() >= inicio_id && processoDTO.getNuProcesso() <= final_id) {
+
+				listProcessoFiltrados.add(processoDTO);
+			}
+		}
+
+		return listProcessoFiltrados;
+	}
+
+	public List<ProcessoDTO> recuperarProcessosMockadosFiltradosPagePageSize(Integer page, Integer page_size) {	
+		
+		return recuperarProcessosMockadosFiltrados(page, page_size);
+	}
+
+	public List<ProcessoDTO> recuperarProcessosMockadosFiltradosOffSetLimit(Integer offset, Integer limit) {
+		return recuperarProcessosMockadosFiltrados(offset, limit);
 	}
 
 }
